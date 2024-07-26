96df5bb2ff3db9feed419eeb9f7aa7b9acc50867
Isaac Caires <zrfisaac@gmail.com>
2024-07-26 07:04:57 -0300

    zrfisaac.web.bash.count : 0.0.1

---
+++ bash/count.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+# - version : zrfisaac.web.bash.count : 0.0.1
+
+# [ bash ]
+for _count in {1..10}
+do
+	echo ${_count}
+done
