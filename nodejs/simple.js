07b2543b0d8cca81e2020f37187058f90e69159e
Isaac Caires <zrfisaac@gmail.com>
2024-07-26 07:26:33 -0300

    zrfisaac.web.nodejs.simple : 0.0.2

--- nodejs/simple.js
+++ nodejs/simple.js
@@ -5,14 +5,34 @@
 // # - author : Isaac Caires
 // # . - email : zrfisaac@gmail.com
 // # . - site : zrfisaac.github.io
-// # - version : zrfisaac.web.nodejs.simple : 0.0.1
+// # - version : zrfisaac.web.nodejs.simple : 0.0.2
 
 // # [ nodejs ]
-http = require('node:http');
-listener = function (request, response) {
-   response.writeHead(200, {'Content-Type': 'text/html'});
-   response.end('<h2 style="text-align: center;">Hello World</h2>');
+
+// # : - variable
+v_http = require('node:http');
+
+// # - function
+function fn_date(date) {
+    const pad = (num, size = 2) => String(num).padStart(size, '0');
+    const year = date.getFullYear();
+    const month = pad(date.getMonth() + 1);
+    const day = pad(date.getDate());
+    const hours = pad(date.getHours());
+    const minutes = pad(date.getMinutes());
+    const seconds = pad(date.getSeconds());
+    const milliseconds = pad(date.getMilliseconds(), 3);
+    return `${year}-${month}-${day} ${hours}.${minutes}.${seconds}.${milliseconds}`;
+}
+
+// # - function
+fn_http = function (_p_request, _p_response) {
+	console.log(`# : request ${fn_date(new Date())}`);
+	_p_response.writeHead(200, {'Content-Type': 'text/html'});
+	_p_response.end('<h2 style="text-align: center;">Hello World</h2>');
 };
-server = http.createServer(listener);
+
+// # - routine
+server = v_http.createServer(fn_http);
 server.listen(3000);
 console.log('Server running at http://127.0.0.1:3000/');
