79fc3545fe14bfc04aa93a63e14505193a964288
Isaac Caires <zrfisaac@gmail.com>
2024-07-26 07:57:52 -0300

    zrfisaac.web.nodejs.callback : 0.0.1

---
+++ nodejs/callback.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// # [ zrfisaac ]
+
+// # [ about ]
+// # - author : Isaac Caires
+// # . - email : zrfisaac@gmail.com
+// # . - site : zrfisaac.github.io
+// # - version : zrfisaac.web.nodejs.callback : 0.0.1
+
+// # [ nodejs ]
+setTimeout(function () { 
+   console.log('This prints after 1000 ms'); 
+}, 1000);
+console.log("Hello World");
