8c594afd449f1775fbdf84797774d0b7298eec89
Isaac Caires <zrfisaac@gmail.com>
2024-07-26 07:04:47 -0300

    zrfisaac.web.bash.config : 0.0.1

---
+++ bash/config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+# - version : zrfisaac.web.bash.config : 0.0.1
+
+# [ bash ]
+
+# - config
+c_path=$(pwd)
