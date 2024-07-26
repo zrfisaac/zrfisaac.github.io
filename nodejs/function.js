d9b80ea6b0345b331057f1e2101bb1973d91ccd1
Isaac Caires <zrfisaac@gmail.com>
2024-07-26 07:57:56 -0300

    zrfisaac.web.nodejs.function : 0.0.1

---
+++ nodejs/function.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// # [ zrfisaac ]
+
+// # [ about ]
+// # - author : Isaac Caires
+// # . - email : zrfisaac@gmail.com
+// # . - site : zrfisaac.github.io
+// # - version : zrfisaac.web.nodejs.function : 0.0.1
+
+// # [ nodejs ]
+function fn_debug(_p_value = "debug") {
+	console.log("# : - begin")
+	console.log("# : - " + _p_value)
+	return 0;
+	console.log("# : - end")
+}
+fn_debug()
