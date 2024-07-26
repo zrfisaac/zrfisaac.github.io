45608d77ede34d60435c2dd8cbc9a4b66d90347f
Isaac Caires <zrfisaac@gmail.com>
2024-07-26 07:05:58 -0300

    zrfisaac.web.bash.void : 0.0.1

---
+++ bash/void.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+# - version : zrfisaac.web.bash.void : 0.0.1
+
+# [ bash ]
