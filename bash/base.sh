def4b250e2fc3236f34ac4f39a68b4fdf0bd3f16
Isaac Caires <zrfisaac@gmail.com>
2024-07-26 07:04:42 -0300

    zrfisaac.web.bash.base : 0.0.1

---
+++ bash/base.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+# - version : zrfisaac.web.bash.base : 0.0.1
+
+# [ bash ]
