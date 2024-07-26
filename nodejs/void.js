0ed7345a9ea5cf50fa72eb977f4064a41bd97fcf
Isaac Caires <zrfisaac@gmail.com>
2024-07-26 07:06:07 -0300

    zrfisaac.web.nodejs.void : 0.0.1

---
+++ nodejs/void.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// # [ zrfisaac ]
+
+// # [ about ]
+// # - author : Isaac Caires
+// # . - email : zrfisaac@gmail.com
+// # . - site : zrfisaac.github.io
+// # - version : zrfisaac.web.nodejs.void : 0.0.1
+
+// # [ nodejs ]
