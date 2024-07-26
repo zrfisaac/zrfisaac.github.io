8aca397815b0bc1d262b750fa2d0e39a78bb2393
Isaac Caires <zrfisaac@gmail.com>
2024-07-26 07:48:36 -0300

    zrfisaac.web.nodejs.environment : 0.0.1

---
+++ nodejs/environment.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// # [ zrfisaac ]
+
+// # [ about ]
+// # - author : Isaac Caires
+// # . - email : zrfisaac@gmail.com
+// # . - site : zrfisaac.github.io
+// # - version : zrfisaac.web.nodejs.environment : 0.0.1
+
+// # [ nodejs ]
+console.log("# $HOME : " + process.env.HOME);
+console.log("# $USER : " + process.env.USER);
