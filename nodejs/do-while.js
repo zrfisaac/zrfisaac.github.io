28532953912f3950554e1c1ca5571a48a1ede7f3
Isaac Caires <zrfisaac@gmail.com>
2024-07-26 07:41:38 -0300

    zrfisaac.web.nodejs.do.while : 0.0.1

---
+++ nodejs/do-while.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// # [ zrfisaac ]
+
+// # [ about ]
+// # - author : Isaac Caires
+// # . - email : zrfisaac@gmail.com
+// # . - site : zrfisaac.github.io
+// # - version : zrfisaac.web.nodejs.do.while : 0.0.1
+
+// # [ nodejs ]
+x=0
+do {
+	x++;
+	console.log("# - x : "+x);
+}
+while (x < 5);
