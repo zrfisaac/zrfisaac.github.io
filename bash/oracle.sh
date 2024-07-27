5cd1163f65a10c7cf6276f918d904f2c83895108
Isaac Caires <zrfisaac@gmail.com>
2024-07-27 09:57:53 -0300

    zrfisaac.web.bash.oracle : 0.0.4

--- bash/oracle.sh
+++ bash/oracle.sh
@@ -5,12 +5,12 @@
 # - author : Isaac Caires
 # . - email : zrfisaac@gmail.com
 # . - site : zrfisaac.github.io
-# - version : zrfisaac.web.bash.oracle : 0.0.2
+# - version : zrfisaac.web.bash.oracle : 0.0.4
 
 # [ bash ]
 
 # - config
-c_sqlplus_shell=$(command -v sqlplus &> /dev/null && echo "sqlplus" || echo "sqlplus")
+c_oracle_shell=$(command -v sqlplus &> /dev/null && echo "sqlplus" || echo "sqlplus")
 c_oracle_server="127.0.0.1"
 c_oracle_port="1521"
 c_oracle_service="XE"
@@ -36,5 +36,6 @@ for _file in $(find "${c_path}" \( -name "*.sql" -o -name "*.oracle.sql" \)); do
 	# sqlplus 'system/"ABcd!@34"@127.0.0.1:1521/XE'
 	_data="system/\"${c_oracle_password}\"@${c_oracle_server}:${c_oracle_port}/${c_oracle_service}"
 	_real="$(realpath ${_file})"
-	echo "@${_real}" | sqlplus -S "${_data}"
+	echo "# : ${_file}"
+	echo "@${_real}" | ${c_oracle_shell} -S "${_data}"
 done
