b3790eefb6453acd7af9f52f668607a0ca77962d
Isaac Caires <zrfisaac@gmail.com>
2024-07-27 07:26:34 -0300

    zrfisaac.web.mysql.data : 0.0.1

---
+++ mysql/data.sql
@@ -0,0 +1,18 @@
+-- # [ zrfisaac ]
+
+-- # [ about ]
+-- # - author : isaac caires
+-- # . - email : zrfisaac@gmail.com
+-- # . - site : zrfisaac.github.io
+-- # - version : zrfisaac.web.mysql.data : 0.0.1
+
+-- # [ mssql ]
+select
+	 0				as `cp_id`
+	,''				as `cp_name`
+where 0 <> 0
+-- 					cp_id		,cp_name
+union all select 	1			,'red'
+union all select 	1			,'green'
+union all select 	1			,'blue'
+;
