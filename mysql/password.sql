658552a548686a1aed0a6fb8e4f634d3f6eb9954
Isaac Caires <zrfisaac@gmail.com>
2024-07-27 07:26:42 -0300

    zrfisaac.web.mysql.password : 0.0.1

---
+++ mysql/password.sql
@@ -0,0 +1,11 @@
+-- # [ zrfisaac ]
+
+-- # [ about ]
+-- # - author : isaac caires
+-- # . - email : zrfisaac@gmail.com
+-- # . - site : zrfisaac.github.io
+-- # - version : zrfisaac.web.mysql.password : 0.0.1
+
+-- # [ mysql ]
+set password for 'root'@'localhost' = password('1234');
+flush privileges;
