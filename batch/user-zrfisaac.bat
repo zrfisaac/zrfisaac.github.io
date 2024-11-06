11b0811c0bff9f571a7fef18528f1ef75f487d7f
Isaac Caires <zrfisaac@gmail.com>
2024-11-06 15:08:10 -0300

    zrfisaac : 1.0.23

---
+++ batch/user-zrfisaac.bat
@@ -0,0 +1,12 @@
+@echo off
+REM [ zrfisaac ]
+
+REM [ about ]
+REM - author : Isaac Caires
+REM . - email : zrfisaac@gmail.com
+REM . - site : zrfisaac.github.io
+REM version : zrfisaac.batch.user.zrfisaac : 1.0.0
+
+REM [ batch ]
+git git config --global user.name "Isaac Caires"
+git git config --global user.email "zrfisaac@gmail.com"
