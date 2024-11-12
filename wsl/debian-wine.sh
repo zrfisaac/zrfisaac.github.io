850c69a00bb77a61f33b4c7539c532b14a0ab036
Isaac Caires <zrfisaac@gmail.com>
2024-11-12 20:24:04 -0300

    zrfisaac : 1.0.42

---
+++ wsl/debian-wine.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+# - version : zrfisaac.wsl.debian.wine : 1.0.0
+
+# [ bash ]
+sudo dpkg --add-architecture i386
+sudo mkdir -pm755 /etc/apt/keyrings
+sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/bookworm/winehq-bookworm.sources
+sudo apt update
+sudo apt install -y gnupg2 software-properties-common wget
+sudo apt install -y --install-recommends winehq-stable
---
+++ wsl/systemd.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+# - version : zrfisaac.debian.sudo : 1.0.0
+
+# [ bash ]
+command -v sudo >/dev/null 2>&1 && sudo="sudo" || sudo=""
+${sudo} echo "[boot]" > /etc/wsl.conf
+${sudo} echo "systemd=true" >> /etc/wsl.conf
