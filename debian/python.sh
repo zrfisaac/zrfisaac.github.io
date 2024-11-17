e8ca103d85af0ca05c3aba69dd11b324a89dd99f
Isaac Caires <zrfisaac@gmail.com>
2024-11-17 17:08:15 -0300

    zrfisaac : 1.0.49

--- debian/python.sh
+++ debian/python.sh
@@ -5,11 +5,12 @@
 # - author : Isaac Caires
 # . - email : zrfisaac@gmail.com
 # . - site : zrfisaac.github.io
-# - version : zrfisaac.debian.python : 1.0.0
+# - version : zrfisaac.debian.python : 1.0.1
 
 # [ bash ]
 command -v sudo >/dev/null 2>&1 && sudo="sudo" || sudo=""
 ${sudo} apt -y update
 ${sudo} apt -y install python3
 ${sudo} apt -y install python3-pip
+${sudo} apt -y install python-is-python3
 ${sudo} [ ! -f "/usr/bin/python" ] && ${sudo} ln -s /usr/bin/python3 /usr/bin/python
--- wsl/systemd.sh
+++ wsl/systemd.sh
@@ -5,7 +5,11 @@
 # - author : Isaac Caires
 # . - email : zrfisaac@gmail.com
 # . - site : zrfisaac.github.io
-# - version : zrfisaac.debian.sudo : 1.0.0
+# - version : zrfisaac.wsl.systemd : 1.0.1
+
+# [ shell ]
+
+# - : wsl --shutdown
 
 # [ bash ]
 command -v sudo >/dev/null 2>&1 && sudo="sudo" || sudo=""
