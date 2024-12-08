ef2e1586300d884a312cedec3221b42cf9e65724
Isaac Caires <zrfisaac@gmail.com>
2024-12-08 07:33:20 -0300

    1.0.77

--- library/local/js/main.js
+++ library/local/js/main.js
@@ -1,13 +1,10 @@
 "use strict";
-
-//Enable tooltips everywhere
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
+	return new bootstrap.Tooltip(tooltipTriggerEl)
 })
-
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  document.body.setAttribute("data-theme", "dark")
+	document.body.setAttribute("data-theme", "dark")
 } else {
-  document.body.removeAttribute("data-theme");
+	document.body.removeAttribute("data-theme");
 }
