53a794c7b74814371bbcc585be96b08f2a6da9dd
Isaac Caires <zrfisaac@gmail.com>
2025-01-08 16:34:28 +0000

    zrfisaac : 3.1.6

--- update.sh
+++ update.sh
@@ -5,8 +5,10 @@
 # - author : Isaac Caires
 # . - email : zrfisaac@gmail.com
 # . - site : zrfisaac.github.io
-# - version : zrfisaac.update : 1.0.0
+# - version : zrfisaac.update : 1.0.1
 
 # [ bash ]
+user=""
+[ "${user}" == "" ] && user=$(getent passwd 1000 | cut -d: -f1)
+[ "${user}" == "" ] && user=$(getent passwd 15443 | cut -d: -f1)
 command -v sudo >/dev/null 2>&1 && sudo="sudo" || sudo=""
-_user=$(getent passwd 1000 | cut -d: -f1)
