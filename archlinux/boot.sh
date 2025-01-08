5c5f488b03551b1d122515b5c0d792157b0e44e9
Isaac Caires <zrfisaac@gmail.com>
2025-01-08 18:12:56 +0000

    zrfisaac : 3.1.9

--- archlinux/boot.sh
+++ archlinux/boot.sh
@@ -12,3 +12,12 @@ user=""
 [ "${user}" == "" ] && user=$(getent passwd 1000 | cut -d: -f1)
 [ "${user}" == "" ] && user=$(getent passwd 15443 | cut -d: -f1)
 command -v sudo >/dev/null 2>&1 && sudo="sudo" || sudo=""
+input=""
+[ "${input}" == "" ] && input="${1}"
+[ "${input}" == "" ] && read input
+if [ "${input}" != "" ]
+then
+	${sudo} mkinitcpio -p linux
+	${sudo} grub-install --target=x86_64-efi --recheck ${input} --efi-directory=/efi --boot-directory=/boot
+	${sudo} grub-mkconfig -o /boot/grub/grub.cfg
+fi
