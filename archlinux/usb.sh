5eef95efd4cfa7c5ef8da0d71700a9fcdb70a2a9
Isaac Caires <zrfisaac@gmail.com>
2025-01-09 01:17:34 +0000

    zrfisaac : 3.1.10

---
+++ archlinux/usb.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+# - version : zrfisaac.archlinux.boot : 1.0.0
+
+# [ bash ]
+user=""
+[ "${user}" == "" ] && user=$(getent passwd 1000 | cut -d: -f1)
+[ "${user}" == "" ] && user=$(getent passwd 15443 | cut -d: -f1)
+command -v sudo >/dev/null 2>&1 && sudo="sudo" || sudo=""
+input=""
+[ "${input}" == "" ] && input="${1}"
+[ "${input}" == "" ] && read input
+if [ "${input}" != "" ]
+then
+	${sudo} mkinitcpio -p linux
+	${sudo} grub-install --target=x86_64-efi --recheck ${input} --efi-directory=/efi --boot-directory=/boot --bootloader-id=GRUB --removable
+	${sudo} grub-mkconfig -o /boot/grub/grub.cfg
+fi
