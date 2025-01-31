e53d5af2f54bd914a627652d642784939b3fb71e
Isaac Caires <zrfisaac@gmail.com>
2025-01-31 06:15:57 -0300

    zrfisaac : 3.1.28

---
+++ manjaro/aseprite.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+# - version : zrfisaac.manjaro.aseprite : 1.0.0
+
+# [ bash ]
+command -v sudo >/dev/null 2>&1 && sudo="sudo" || sudo=""
+yay -S --noconfirm aseprite
