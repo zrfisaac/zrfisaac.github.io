37a2f2d24396e92a964cdd7a6d24918e60cc09c5
Isaac Caires <zrfisaac@gmail.com>
2025-02-09 08:55:22 +0000

    zrfisaac.wine.build : 0.0.1

---
+++ wine/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+# - version : zrfisaac.wine.build : 0.0.1
+
+# [ bash ]
+user=""
+[ "${user}" == "" ] && user=$(getent passwd 1000 | cut -d: -f1)
+[ "${user}" == "" ] && user=$(getent passwd 15443 | cut -d: -f1)
+command -v sudo >/dev/null 2>&1 && sudo="sudo" || sudo=""
+./configure
+make -j$(nproc)
