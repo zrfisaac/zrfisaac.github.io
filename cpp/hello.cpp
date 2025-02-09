4747cdd3d236124860b6f5484ee995384cb6504f
Isaac Caires <zrfisaac@gmail.com>
2025-02-09 17:56:10 +0000

    zrfisaac.cpp.hello : 0.0.1

---
+++ cpp/hello.cpp
@@ -0,0 +1,15 @@
+// # [ zrfisaac ]
+
+// # [ about ]
+// # - author : Isaac Caires
+// # . - email : zrfisaac@gmail.com
+// # . - site : zrfisaac.github.io
+// # - version : zrfisaac.cpp.hello : 0.0.1
+
+// # [ cpp ]
+#include <iostream>
+
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
