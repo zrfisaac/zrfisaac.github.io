85133472eb5a1b5fb028a9d2e62229f60552da98
Isaac Caires <zrfisaac@gmail.com>
2025-02-09 17:57:15 +0000

    zrfisaac.rust.hello : 0.0.1

---
+++ rust/hello.rs
@@ -0,0 +1,12 @@
+// # [ zrfisaac ]
+
+// # [ about ]
+// # - author : Isaac Caires
+// # . - email : zrfisaac@gmail.com
+// # . - site : zrfisaac.github.io
+// # - version : zrfisaac.rust.hello : 0.0.1
+
+// # [ rust ]
+fn main() {
+	println!("Hello, World!");
+}
