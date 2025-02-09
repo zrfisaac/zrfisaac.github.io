7ca32c7dbd9b33f0deb561373f5124999b4f94b1
Isaac Caires <zrfisaac@gmail.com>
2025-02-09 11:38:14 +0000

    zrfisaac.ollama : 0.0.2

--- ollama.sh
+++ ollama.sh
@@ -5,7 +5,7 @@
 # - author : Isaac Caires
 # . - email : zrfisaac@gmail.com
 # . - site : zrfisaac.github.io
-# - version : zrfisaac.ollama : 0.0.1
+# - version : zrfisaac.ollama : 0.0.2
 
 # [ bash ]
 user=""
@@ -14,11 +14,13 @@ user=""
 command -v sudo >/dev/null 2>&1 && sudo="sudo" || sudo=""
 if [ -x "$(which ollama)" ]
 then
-	# :   The latest series of Code-Specific Qwen models, with
-	# : significant improvements in code generation, code reasoning,
-	# : and code fixing.
-	ollama pull qwen2.5-coder:0.5b
-	ollama pull qwen2.5-coder:1.5b
+	# :   DeepSeek's first-generation of reasoning models with
+	# : comparable performance to OpenAI-o1, including six dense models
+	# : distilled from DeepSeek-R1 based on Llama and Qwen.
+	ollama pull deepseek-r1:1.5b
+
+	# :   Meta's Llama 3.2 goes small with 1B and 3B models.
+	ollama pull llama3.2:1b
 
 	# :   Qwen2.5 models are pretrained on Alibaba's latest large-scale
 	# : dataset, encompassing up to 18 trillion tokens.
@@ -26,4 +28,10 @@ then
 	# : support.
 	ollama pull qwen2.5:0.5b
 	ollama pull qwen2.5:1.5b
+
+	# :   The latest series of Code-Specific Qwen models, with
+	# : significant improvements in code generation, code reasoning,
+	# : and code fixing.
+	ollama pull qwen2.5-coder:0.5b
+	ollama pull qwen2.5-coder:1.5b
 fi
