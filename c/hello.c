387315868256a371f25ff7eb3363b786a27e66fb
Isaac Caires <zrfisaac@gmail.com>
2025-02-09 17:56:06 +0000

    zrfisaac.c.hello : 0.0.1

---
+++ c/hello.c
@@ -0,0 +1,15 @@
+// # [ zrfisaac ]
+
+// # [ about ]
+// # - author : Isaac Caires
+// # . - email : zrfisaac@gmail.com
+// # . - site : zrfisaac.github.io
+// # - version : zrfisaac.c.hello : 0.0.1
+
+// # [ cpp ]
+#include <stdio.h>
+
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
