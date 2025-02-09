29479292a981dc33657e8c474c06ff279ff7e508
Isaac Caires <zrfisaac@gmail.com>
2025-02-09 17:57:08 +0000

    zrfisaac.python.hello : 0.0.1

---
+++ python/hello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+# - version : zrfisaac.python.hello : 0.0.1
+
+# [ python ]
+print("Hello, World!")
