e72dd1fbe17dc1a868b3715cbc4c1899dab721dc
Isaac Caires <zrfisaac@gmail.com>
2025-02-11 21:37:21 +0000

    zrfisaac.bash.hello : 0.0.1

---
+++ bash/hello.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+# - version : zrfisaac.bash.hello : 0.0.1
+
+# [ bash ]
+echo "Hello, World!"
