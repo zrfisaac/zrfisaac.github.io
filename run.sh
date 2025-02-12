3164fd660cdaff59b6620cb2160594fe8621172d
Isaac Caires <zrfisaac@gmail.com>
2025-02-12 19:03:01 +0000

    zrfisaac.run : 0.0.2

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@
 # - author : Isaac Caires
 # . - email : zrfisaac@gmail.com
 # . - site : zrfisaac.github.io
-# version : zrfisaac.run : 0.0.1
+# version : zrfisaac.run : 0.0.2
 
 # [ bash ]
 echo '#!/data/data/com.termux/files/usr/bin/sh' > ~/.vnc/xstartup
@@ -14,7 +14,9 @@ echo 'unset SESSION_MANAGER' >> ~/.vnc/xstartup
 echo 'unset DBUS_SESSION_BUS_ADDRESS' >> ~/.vnc/xstartup
 echo 'dbus-launch --exit-with-session xfce4-session &' >> ~/.vnc/xstartup
 [ ! -d "~/novnc" ] && git clone https://github.com/novnc/noVNC.git ~/novnc
+[ -d "~/novnc" ] && [ ! -d "~/novnc/utils/websockify" ] && git clone https://github.com/novnc/websockify.git ~/novnc/utils/websockify
 vncserver -kill :1
-vncserver :1
+vncserver :1 &
 chmod +x ./novnc/utils/novnc_proxy
-./novnc/utils/novnc_proxy --vnc localhost:5901 --listen 0.0.0.0:15443
+./novnc/utils/novnc_proxy --vnc localhost:5901 --listen 0.0.0.0:15443 &
+ifconfig
