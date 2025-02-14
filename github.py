3bd8411bf3d3f8d5f434539d55f0ab6bd56f5e7b
Isaac Caires <zrfisaac@gmail.com>
2025-02-14 15:22:59 -0300

    zrfisaac.github.py : 1.0.0

---
+++ github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+# version : zrfisaac.github.py : 1.0.0
+
+# [ python ]
+import subprocess
+subprocess.run(["git", "config", "--global", "user.name", "Isaac Caires"])
+subprocess.run(["git", "config", "--global", "user.email", "zrfisaac@gmail.com"])
