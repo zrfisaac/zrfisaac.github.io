4e2933fe25b678e01276f3e7d4c660a6a7a375f5
Isaac Caires <zrfisaac@gmail.com>
2025-02-14 15:02:04 -0300

    zrfisaac.batch.void : 0.0.1

---
+++ batch/void.bat
@@ -0,0 +1,11 @@
+@echo off
+setlocal enabledelayedexpansion
+REM [ zrfisaac ]
+
+REM [ about ]
+REM - author : Isaac Caires
+REM . - email : zrfisaac@gmail.com
+REM . - site : zrfisaac.github.io
+REM version : zrfisaac.batch.void : 0.0.1
+
+REM [ batch ]
