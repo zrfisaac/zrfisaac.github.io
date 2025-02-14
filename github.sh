186433af4edfee39f0e495459fa05228df29d794
Isaac Caires <zrfisaac@gmail.com>
2025-02-14 15:23:04 -0300

    zrfisaac.github.sh : 1.0.0

---
+++ github.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+# - version : zrfisaac.github.sh : 1.0.0
+
+# [ bash ]
+git config --global user.name "Isaac Caires"
+git config --global user.email "zrfisaac@gmail.com"
+git config --global user.name
+git config --global user.email
