2ba7ec09ff0c82353fdfd7cf132a026739fac8f2
Isaac Caires <zrfisaac@gmail.com>
2025-02-14 15:22:53 -0300

    zrfisaac.github.bat : 0.0.1

---
+++ github.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal enabledelayedexpansion
+REM [ zrfisaac ]
+
+REM [ about ]
+REM - author : Isaac Caires
+REM . - email : zrfisaac@gmail.com
+REM . - site : zrfisaac.github.io
+REM version : zrfisaac.github.bat : 0.0.1
+
+REM [ batch ]
+git config --global user.name "Isaac Caires"
+git config --global user.email "zrfisaac@gmail.com"
+git config --global user.name
+git config --global user.email
+echo # [ end ]
+set /p _=
