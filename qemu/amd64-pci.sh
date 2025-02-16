9efa2b43e6361852588dd7000f88287b9b0e1537
Isaac Caires <zrfisaac@gmail.com>
2025-02-16 14:56:38 +0000

    zrfisaac.qemu.amd64.pci : 0.0.1

---
+++ qemu/amd64-pci.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+# version : zrfisaac.qemu.amd64.pci : 0.0.1
+
+# [ bash ]
+file=$(basename "$0")
+file="${file%.*}"
+[ -f "${file}.img" ] && file="${file}.img"
+[ -f "${file}.qcow2" ] && file="${file}.qcow2"
+if [ -f "${file}" ]
+then
+	qemu-system-x86_64 \
+	  -drive file=${file},format=raw \
+	  -m 4G \
+	  -enable-kvm \
+	  -cpu host \
+	  -smp 4 \
+	  -virtfs local,path=/zrfisaac/public,mount_tag=public,security_model=none,id=fs0 \
+	  -net nic \
+	  -net user,hostfwd=tcp::3389-:3389 \
+	  -device virtio-gpu-pci
+fi
