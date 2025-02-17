b82814f798c31c010b18f3a82f9ab09fc4a7b50f
Isaac Caires <zrfisaac@gmail.com>
2025-02-17 09:35:29 +0000

    zrfisaac.debian.winetricks : 0.0.1

---
+++ debian/winetricks.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+# - version : zrfisaac.debian.winetricks : 0.0.1
+
+# [ bash ]
+command -v sudo >/dev/null 2>&1 && sudo="sudo" || sudo=""
+[ -f "/usr/bin/winetricks" ] && ${sudo} rm -rf /usr/bin/winetricks
+${sudo} wget -O /usr/bin/winetricks https://raw.githubusercontent.com/Winetricks/winetricks/refs/heads/master/src/winetricks
+${sudo} chmod +x /usr/bin/winetricks
