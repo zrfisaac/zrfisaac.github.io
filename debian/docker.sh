10498df19e174b798bc9550c5f6aee45fa744e51
Isaac Caires <zrfisaac@gmail.com>
2025-02-17 09:34:39 +0000

    zrfisaac.debian.docker : 0.0.1

---
+++ debian/docker.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+# - version : zrfisaac.debian.docker : 0.0.1
+
+# [ bash ]
+command -v sudo >/dev/null 2>&1 && sudo="sudo" || sudo=""
+${sudo} apt -y update
+${sudo} apt -y install apt-transport-https ca-certificates curl software-properties-common
+[ ! -f "/usr/share/keyrings/docker-archive-keyring.gpg" ] &&  ${sudo} curl -fsSL https://download.docker.com/linux/debian/gpg | ${sudo}  gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+[ ! -f "/etc/apt/sources.list.d/docker.list" ] && ${sudo} echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | ${sudo} tee /etc/apt/sources.list.d/docker.list > /dev/null
+${sudo} apt -y update
+${sudo} apt -y install docker-ce
+${sudo} systemctl start docker
+${sudo} systemctl enable docker
