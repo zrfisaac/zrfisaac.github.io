ff1fe130c862a0bcdfbd5d6f86a036e4daabe68f
Isaac Caires <zrfisaac@gmail.com>
2025-02-17 09:35:34 +0000

    zrfisaac.wine.mssql : 0.0.1

---
+++ wine/mssql.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+# - version : zrfisaac.wine.mssql : 0.0.1
+
+# [ bash ]
+winetricks --force --unattended mdac28
+wine winecfg -v win10
