a7972730b3badcd9f3c8f3f19ea768dabcb41817
Isaac Caires <zrfisaac@gmail.com>
2025-02-18 19:14:35 -0300

    zrfisaac.mssql.dummy : 0.0.1

---
+++ mssql/dummy.sql
@@ -0,0 +1,10 @@
+-- # [ zrfisaac ]
+
+-- # [ about ]
+-- # - author : Isaac Caires
+-- # . - email : zrfisaac@gmail.com
+-- # . - site : zrfisaac.github.io
+-- # - version : zrfisaac.mssql.dummy : 0.0.1
+
+-- # [ mssql ]
+SELECT 'DUMMY' AS DUMMY
