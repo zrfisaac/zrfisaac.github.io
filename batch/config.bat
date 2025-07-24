85d67635543aca4e5020eb534965ee7e9edfd14e
Isaac Caires Santana <zrfisaac@gmail.com>
2025-07-24 08:55:03 -0300

    zrfisaac.batch.config : 25.7.24.1

---
+++ batch/config.bat
@@ -0,0 +1,17 @@
+@echo off
+rem # [ zrfisaac ]
+
+rem # [ about ]
+rem # - author : Isaac Caires Santana
+rem # . - email : zrfisaac@gmail.com
+rem # . - site : zrfisaac.github.io
+rem # - version : zrfisaac.batch.config : 25.7.24.1
+
+rem # [ batch ]
+
+rem # - config - mssql
+set c_mssql_shell=SQLCMD.exe
+set c_mssql_server=localhost
+set c_mssql_database=DEBUG
+set c_mssql_user=debug
+set c_mssql_password=1234
