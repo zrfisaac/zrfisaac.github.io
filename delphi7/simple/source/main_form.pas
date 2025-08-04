82717a3f1147f7c1f22041c1cd899832ca77dace
Isaac Caires Santana <zrfisaac@gmail.com>
2025-08-04 10:21:07 -0300

    zrfisaac.delphi7.simple : 25.8.4.1

---
+++ delphi7/simple/source/main_form.pas
@@ -0,0 +1,40 @@
+unit main_form;
+
+interface
+
+uses
+  Windows,
+  Messages,
+  SysUtils,
+  Variants,
+  Classes,
+  Graphics,
+  Controls,
+  Forms,
+  Dialogs,
+  ComCtrls;
+
+type
+  TMainForm = class(TForm)
+    sbFooter: TStatusBar;
+    sbBack: TScrollBox;
+    procedure FormCreate(Sender: TObject);
+  end;
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+uses
+  main_data;
+
+{$R *.dfm}
+
+procedure TMainForm.FormCreate(Sender: TObject);
+begin
+  // # : - title
+  Self.Caption := Application.Title;
+end;
+
+end.
