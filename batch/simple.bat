c91709a0e6381ed9e430cea1fe5b55b413f63877
Isaac Caires Santana <zrfisaac@gmail.com>
2025-08-04 10:56:51 -0300

    zrfisaac.batch.simple : 25.8.4.1

---
+++ batch/simple.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+setlocal enabledelayedexpansion
+chcp 65001 >NUL
+rem # [ zrfisaac ]
+
+rem # [ about ]
+rem # - author : Isaac Caires Santana
+rem # - email : zrfisaac@gmail.com
+rem # - site : zrfisaac.github.io
+rem # - version : zrfisaac.batch.simple : 25.8.4.1
+
+rem # [ batch ]
+
+rem # : - end
+echo # [ end ]
+set /p _=
