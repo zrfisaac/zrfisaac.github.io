21aa30e4c7704aa055396f4098ff87f38a767a1b
Isaac Caires Santana <zrfisaac@gmail.com>
2025-08-04 10:57:10 -0300

    zrfisaac.python.simple : 25.8.4.1

---
+++ python/simple.py
@@ -0,0 +1,13 @@
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires Santana
+# - email : zrfisaac@gmail.com
+# - site : zrfisaac.github.io
+# - version : zrfisaac.python.simple : 25.8.4.1
+
+# [ python ]
+
+# : - end
+print("# [ end ]")
+input()
