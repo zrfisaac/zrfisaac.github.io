a39957191f53fe5d95c0e458a3d732c90599851c
Isaac Caires Santana <zrfisaac@gmail.com>
2025-08-04 10:57:05 -0300

    zrfisaac.batch.special : 25.8.4.1

---
+++ batch/special.bat
@@ -0,0 +1,31 @@
+@echo off
+setlocal
+setlocal enabledelayedexpansion
+chcp 65001 >NUL
+rem # [ zrfisaac ]
+
+rem # [ about ]
+rem # - author : Isaac Caires Santana
+rem # - email : zrfisaac@gmail.com
+rem # - site : zrfisaac.github.io
+rem # - version : zrfisaac.batch.special : 25.8.4.1
+
+rem # [ batch ]
+
+rem # : - special
+set s_double_quotes="
+set s_single_quotes='
+set s_asterisk=*
+set s_caret=^^
+set s_space= 
+set s_equals=^=
+set s_greater=^>
+set s_plus=+
+set s_less=^<
+set s_minus=-
+set s_percentage=%%
+set s_tab=	
+
+rem # : - end
+echo # [ end ]
+set /p _=
