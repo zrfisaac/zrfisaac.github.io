d7e6bc8da205087062d0314a9866b8840f6f8689
Isaac Caires Santana <zrfisaac@gmail.com>
2025-08-17 14:20:15 -0300

    zrfisaac : 25.8.17.5

---
+++ database/debug/config.bat
@@ -0,0 +1,25 @@
+@echo off
+rem # [ zrfisaac ]
+
+rem # [ about ]
+rem # - author : Isaac Caires Santana
+rem # - email : zrfisaac@gmail.com
+rem # - site : zrfisaac.github.io
+rem # - version : zrfisaac.batch.config : 25.8.4.1
+
+rem # [ batch ]
+
+rem # - config - mssql
+set c_mssql_server=localhost
+set c_mssql_user=debug
+set c_mssql_password=1234
+set c_mssql_database=DEBUG
+
+rem # - config - mysql
+set c_mysql_server=localhost
+set c_mysql_user=debug
+set c_mysql_password=1234
+set c_mysql_database=debug
+
+rem # - config - mysql
+set c_mysql_database=debug.db
