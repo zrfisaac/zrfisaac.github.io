253a66e0cee2179e2a1d04afbd489fcbff0820fa
Isaac Caires Santana <zrfisaac@gmail.com>
2025-08-23 08:26:15 -0300

    zrfisaac : 25.8.23.1

---
+++ batch/episodio.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal enabledelayedexpansion
+rem # [ zrfisaac ]
+
+rem # [ about ]
+rem # - author : Isaac Caires Santana
+rem # - email : zrfisaac@gmail.com
+rem # - site : zrfisaac.github.io
+rem # - version : zrfisaac.batch.episodio : 25.8.23.1
+
+rem # [ batch ]
+set /a _result=0
+set _number=
+for %%z in (*!end!) do (
+	set /a _result=!_result! + 1
+	set _number=00!_result!
+	set _number=!_number:~-2!
+	if not exist "Episodio!_number!!end!" ren %%z Episodio!_number!!end!
+)
+pause
