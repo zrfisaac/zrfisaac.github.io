212a4fc46533f7fce5f8db619fa16a8b24bc2d63
Isaac Caires Santana <zrfisaac@gmail.com>
2025-10-15 13:04:02 -0300

    zrfisaac.batch.hello : 25.10.15.1

---
+++ batch/hello.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal enabledelayedexpansion
+chcp 65001 >nul
+rem [ zrfisaac ]
+
+rem # [ about ]
+rem # - author : Isaac Caires Santana
+rem # - email : zrfisaac@gmail.com
+rem # - site : zrfisaac.github.io
+rem # - version : zrfisaac.batch.hello : 25.10.15.1
+
+rem # [ batch ]
+echo Hello, World^^!
+set /p _end=
+echo.
