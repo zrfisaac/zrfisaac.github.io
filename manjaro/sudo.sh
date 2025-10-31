4990402a532574cb8ec6e3c001e1ebca88d12710
Isaac Caires Santana <zrfisaac@gmail.com>
2025-10-31 10:19:35 -0300

    zrfisaac.manjaro.sudo : 25.10.31.1

---
+++ manjaro/sudo.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires Santana
+# - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+# - version : zrfisaac.manjaro.sudo : 25.10.31.1
+
+# [ bash ]
+command -v sudo >/dev/null 2>&1 && sudo="sudo" || sudo=""
+_user=$(getent passwd 1000 | cut -d: -f1)
+${sudo} echo "${_user} ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/${_user}
