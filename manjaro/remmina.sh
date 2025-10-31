5358048c8a00e1b2cd43d1f269bc36138a5e1c10
Isaac Caires Santana <zrfisaac@gmail.com>
2025-10-31 10:19:02 -0300

    zrfisaac.manjaro.remmina : 25.10.31.1

---
+++ manjaro/remmina.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires Santana
+# - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+# - version : zrfisaac.manjaro.remmina : 25.10.31.1
+
+# [ bash ]
+command -v sudo >/dev/null 2>&1 && sudo="sudo" || sudo=""
+${sudo} pacman -S --noconfirm remmina
+${sudo} pacman -S --noconfirm libvncserver
+${sudo} pacman -S --noconfirm gtk-vnc
+${sudo} pacman -S --noconfirm freerdp
+${sudo} pacman -S --noconfirm freerdp
+${sudo} pacman -S --noconfirm openssh
+${sudo} pacman -S --noconfirm tigervnc
