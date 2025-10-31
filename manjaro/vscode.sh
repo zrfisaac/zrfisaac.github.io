bf7d8a397558e5d79d7fd7da5a7304b25ae66dc4
Isaac Caires Santana <zrfisaac@gmail.com>
2025-10-31 10:19:58 -0300

    version : zrfisaac.manjaro.vscode : 25.10.31.1

---
+++ manjaro/vscode.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires Santana
+# - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+# - version : zrfisaac.manjaro.vscode : 25.10.31.1
+
+# [ bash ]
+command -v sudo >/dev/null 2>&1 && sudo="sudo" || sudo=""
+yay -S --noconfirm visual-studio-code-bin
