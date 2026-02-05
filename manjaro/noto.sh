bff75bab8a93360f99aa625b49a3f535e4d44835
Isaac Caires Santana <zrfisaac@gmail.com>
2026-02-05 19:32:22 -0300

    zrfisaac.manjaro.noto : 26.2.5.1

---
+++ manjaro/noto.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires Santana
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+# - version : zrfisaac.manjaro.noto : 26.2.5.1
+
+# [ bash ]
+user=$(getent passwd 1000 | cut -d: -f1)
+command -v sudo >/dev/null 2>&1 && sudo="sudo" || sudo=""
+${sudo} pacman -S --noconfirm noto-fonts
+${sudo} pacman -S --noconfirm noto-fonts-cjk
+${sudo} pacman -S --noconfirm noto-fonts-emoji
