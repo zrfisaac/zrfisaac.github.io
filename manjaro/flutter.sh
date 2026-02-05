d0f9c35c633097b303dcdca800936fe3d40cd2ec
Isaac Caires Santana <zrfisaac@gmail.com>
2026-02-05 19:31:32 -0300

    zrfisaac.manjaro.flutter : 26.2.5.1

---
+++ manjaro/flutter.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires Santana
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+# - version : zrfisaac.manjaro.flutter : 26.2.5.1
+
+# [ bash ]
+user=$(getent passwd 1000 | cut -d: -f1)
+command -v sudo >/dev/null 2>&1 && sudo="sudo" || sudo=""
+yay -S --noconfirm flutter-bin
+yay -S --noconfirm flutter
+${sudo} chown -R ${user}:${user} /opt
