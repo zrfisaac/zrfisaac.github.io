6324ac8dbd4f95324bb824d582e2008ea4f361bf
Isaac Caires Santana <zrfisaac@gmail.com>
2026-02-05 19:31:40 -0300

    zrfisaac.manjaro.geany : 26.2.5.1

---
+++ manjaro/geany.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires Santana
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+# - version : zrfisaac.manjaro.geany : 26.2.5.1
+
+# [ bash ]
+user=$(getent passwd 1000 | cut -d: -f1)
+command -v sudo >/dev/null 2>&1 && sudo="sudo" || sudo=""
+${sudo} pacman -S --noconfirm geany
+yay -S --noconfirm geany-themes
