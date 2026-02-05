6fce5dacc7492707fe9a737c952aa494c765c7f1
Isaac Caires Santana <zrfisaac@gmail.com>
2026-02-05 19:30:58 -0300

    zrfisaac.manjaro.dbeaver : 26.2.5.1

---
+++ manjaro/dbeaver.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires Santana
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+# - version : zrfisaac.manjaro.dbeaver : 26.2.5.1
+
+# [ bash ]
+user=$(getent passwd 1000 | cut -d: -f1)
+command -v sudo >/dev/null 2>&1 && sudo="sudo" || sudo=""
+${sudo} pacman -S --noconfirm dbeaver
