28466b632f11003ed32e0c55b992af916d97ed15
Isaac Caires Santana <zrfisaac@gmail.com>
2026-02-06 06:43:21 -0300

    zrfisaac.library.local : 26.2.6.1

---
+++ library/local/js/locale.js
@@ -0,0 +1,13 @@
+// # [ zrfisaac ]
+
+// # [ about ]
+// # - author : Isaac Caires
+// # . - email : zrfisaac@gmail.com
+// # . - site : zrfisaac.github.io
+
+// # [ javascript ]
+var _lang = navigator.language || navigator.userLanguage; 
+_lang = _lang.substr(0,2);
+if (!["en", "es", "it", "pt", "ja", "zh"].includes(_lang)) {
+	_lang="en"
+}
