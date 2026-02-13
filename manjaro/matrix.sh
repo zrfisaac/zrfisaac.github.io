059ef484ec209e25a38c27916c44c034528be5bd
Isaac Caires Santana <zrfisaac@gmail.com>
2026-02-13 18:43:49 -0300

    zrfisaac.manjaro.matrix : 26.2.13.1

---
+++ manjaro/matrix.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires Santana
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+# - version : zrfisaac.manjaro.matrix : 26.2.13.1
+
+# [ bash ]
+user=$(getent passwd 1000 | cut -d: -f1)
+command -v sudo >/dev/null 2>&1 && sudo="sudo" || sudo=""
+${sudo} pacman -S --noconfirm cmatrix
