37a9c64ebd1cca0984d295ee4cee49e69c804890
Isaac Caires Santana <zrfisaac@gmail.com>
2026-02-17 07:11:47 -0300

    zrfisaac.bash.base : 26.2.17.1

---
+++ bash/bash.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires Santana
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+# - version : zrfisaac.bash.base : 26.2.17.1
+
+# [ bash ]
+
+# : - begin
+echo "# - : $(realpath "$0")"
+
+# : - end
+echo "# . - end"
+#read -p "" _
