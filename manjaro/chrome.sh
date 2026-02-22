ce81cd0837964e9965594c7acf4b1230d2534aee
Isaac Caires Santana <zrfisaac@gmail.com>
2026-02-22 10:01:21 -0300

    zrfisaac.manjaro.chrome : 26.2.22.1

--- manjaro/chrome.sh
+++ manjaro/chrome.sh
@@ -5,9 +5,10 @@
 # - author : Isaac Caires Santana
 # . - email : zrfisaac@gmail.com
 # . - site : zrfisaac.github.io
-# - version : zrfisaac.manjaro.chrome : 26.2.5.1
+# - version : zrfisaac.manjaro.chrome : 26.2.22.1
 
 # [ bash ]
 user=$(getent passwd 1000 | cut -d: -f1)
 command -v sudo >/dev/null 2>&1 && sudo="sudo" || sudo=""
 yay -S --noconfirm google-chrome
+[ -f "/usr/bin/google-chrome" ] && ${sudo} ln -s /usr/bin/google-chrome-stable /usr/bin/google-chrome
