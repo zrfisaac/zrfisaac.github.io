b621bc2e55ca8cd7ec9b473c8a803742d9267a5f
Isaac Caires Santana <zrfisaac@gmail.com>
2026-02-22 09:16:59 -0300

    zrfisaac.bash.keystore : 26.2.22.1

---
+++ bash/keystore.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires Santana
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+# - version : zrfisaac.bash.keystore : 26.2.22.1
+
+# [ bash ]
+keytool -keyalg RSA -genkeypair -alias androiddebugkey -keypass android -keystore debug.keystore -storepass android -dname "CN=Android Debug,O=Android,C=US" -validity 9999
