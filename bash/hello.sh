3d3225bb6069e107b84cf48bb00896e945244c6d
Isaac Caires Santana <zrfisaac@gmail.com>
2026-02-22 09:16:44 -0300

    zrfisaac.bash.hello : 26.2.22.1

---
+++ bash/hello.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires Santana
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+# - version : zrfisaac.bash.hello : 26.2.22.1
+
+# [ bash ]
+echo "Hello, World!"
