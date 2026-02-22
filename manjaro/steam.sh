13503c64ff70a868d2094dab9d46287aee756d5e
Isaac Caires Santana <zrfisaac@gmail.com>
2026-02-22 10:02:39 -0300

    zrfisaac.manjaro.steam : 26.2.22.1

---
+++ manjaro/steam.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires Santana
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+# - version : zrfisaac.manjaro.steam : 26.2.22.1
+
+# [ bash ]
+user=$(getent passwd 1000 | cut -d: -f1)
+command -v sudo >/dev/null 2>&1 && sudo="sudo" || sudo=""
+${sudo} pacman -S --noconfirm steam
+${sudo} pacman -S --noconfirm game-devices-udev
