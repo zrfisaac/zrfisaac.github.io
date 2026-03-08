e1440f68ab8727fc54831d01694d317c6c4a75bb
Isaac Caires Santana <zrfisaac@gmail.com>
2026-03-08 07:58:04 -0300

    zrfisaac.batch.config : 26.3.6.1

--- batch/config.bat
+++ batch/config.bat
@@ -3,9 +3,9 @@ rem # [ zrfisaac ]
 
 rem # [ about ]
 rem # - author : Isaac Caires Santana
-rem # - email : zrfisaac@gmail.com
-rem # - site : zrfisaac.github.io
-rem # - version : zrfisaac.batch.config : 25.8.4.1
+rem # . - email : zrfisaac@gmail.com
+rem # . - site : zrfisaac.github.io
+rem # - version : zrfisaac.batch.config : 26.3.6.1
 
 rem # [ batch ]
 
@@ -17,7 +17,9 @@ set c_chocolatey=!c_chocolatey! yt-dlp
 set c_chocolatey=!c_chocolatey! imagemagick
 
 rem # - config - mssql
+set c_mssql_shell=SQLCMD.exe
 set c_mssql_server=localhost
-set c_mssql_user=sa
+set c_mssql_user=debug
 set c_mssql_password=1234
-set c_mssql_database=master
+set c_mssql_database=DEBUG
+set c_mssql_target=C:\Bancos
