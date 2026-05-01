ebaa0ccfbda9b7e81c8d5f25a7876ae418767bd2
Isaac Caires Santana <zrfisaac@gmail.com>
2026-05-01 07:11:50 -0300

    zrfisaac.batch.get : 26.5.1.1

---
+++ batch/get.bat
@@ -0,0 +1,20 @@
+@echo off
+chcp 65001 >nul
+setlocal enabledelayedexpansion
+rem # [ zrfisaac ]
+
+rem # [ about ]
+rem # - author : Isaac Caires Santana
+rem # . - email : zrfisaac@gmail.com
+rem # . - site : zrfisaac.github.io
+rem # - version : zrfisaac.batch.get : 26.5.1.1
+
+rem # [ batch ]
+
+rem # : - begin
+echo # - : %~dpnx0
+git clone https://github.com/zrfisaac/zrfisaac.github.io.git %~d0\zrfisaac
+
+rem # : - end
+echo # . - end
+set /p _=
