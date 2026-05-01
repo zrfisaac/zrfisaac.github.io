60a228c5163f0e1cacc78ddef9991d3fdc3f27cc
Isaac Caires Santana <zrfisaac@gmail.com>
2026-05-01 07:49:12 -0300

    zrfisaac : 26.5.1.5

--- config.bat
+++ config.bat
@@ -13,3 +13,4 @@ rem # : - clone
 set c_clone=
 set c_clone=%c_clone% "https://github.com/zrfisaac/zrfisaac.git ./profile"
 set c_clone=%c_clone% "https://github.com/zrfisaac/folder.music.git ./music"
+set c_clone=%c_clone% "https://github.com/zrfisaac/template.docsify.git ./template/docsify"
