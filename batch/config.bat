656fb95cd2cb006247259a72fd108a093298bbe7
Isaac Caires Santana <zrfisaac@gmail.com>
2026-05-01 07:34:34 -0300

    zrfisaac.batch.config : 26.5.1.1

---
+++ batch/config.bat
@@ -0,0 +1,13 @@
+@echo off
+rem # [ zrfisaac ]
+
+rem # [ about ]
+rem # - author : Isaac Caires Santana
+rem # . - email : zrfisaac@gmail.com
+rem # . - site : zrfisaac.github.io
+rem # - version : zrfisaac.batch.config : 26.5.1.1
+
+rem # [ batch ]
+
+rem # : - config
+set c_clone=
