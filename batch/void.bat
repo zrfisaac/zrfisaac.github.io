aad5561cfd8d069caf62ed562446821c4164758d
Isaac Caires Santana <zrfisaac@gmail.com>
2026-05-19 19:27:36 -0300

    zrfisaac.batch.void : 26.5.19.1

---
+++ batch/void.bat
@@ -0,0 +1,12 @@
+@echo off
+chcp 65001 >nul
+setlocal enabledelayedexpansion
+rem # [ zrfisaac ]
+
+rem # [ about ]
+rem # - author : Isaac Caires Santana
+rem # . - email : zrfisaac@gmail.com
+rem # . - site : zrfisaac.github.io
+rem # - version : zrfisaac.batch.void : 26.5.19.1
+
+rem # [ batch ]
