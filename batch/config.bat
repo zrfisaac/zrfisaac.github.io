03b43e043726a17064c46bd43612b6ab4facae3d
Isaac Caires Santana <zrfisaac@gmail.com>
2026-08-30 14:20:01 -0300

    zrfisaac.batch.config : 26.8.30.2

--- batch/config.bat
+++ batch/config.bat
@@ -5,12 +5,13 @@ rem # [ about ]
 rem # - author : Isaac Caires Santana
 rem # . - email : zrfisaac@gmail.com
 rem # . - site : zrfisaac.github.io
-rem # - base : zrfisaac.batch.config : 26.8.30.1
+rem # - base : zrfisaac.batch.config : 26.8.30.2
 
 rem # [ batch ]
 
 rem # : - config - clone
 set c_clone=
-set c_clone=%c_clone% "https://github.com/zrfisaac/wiki.git %~dp0wiki"
 set c_clone=%c_clone% "https://github.com/zrfisaac/zrfisaac.git %~dp0github"
+set c_clone=%c_clone% "https://github.com/zrfisaac/zrfisaac.git %~dp0private"
 set c_clone=%c_clone% "https://github.com/zrfisaac/zrfisaac.github.io.git %~dp0web"
+set c_clone=%c_clone% "https://github.com/zrfisaac/wiki.git %~dp0wiki"
