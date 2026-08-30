91fb5e128d2bfcb8d5a5decb305b9cd0df98374c
Isaac Caires Santana <zrfisaac@gmail.com>
2026-08-30 14:19:51 -0300

    zrfisaac.batch.all : 26.8.30.3

--- all.bat
+++ all.bat
@@ -7,27 +7,44 @@ rem # [ about ]
 rem # - author : Isaac Caires Santana
 rem # . - email : zrfisaac@gmail.com
 rem # . - site : zrfisaac.github.io
-rem # - version : zrfisaac.batch.all : 26.8.30.2
+rem # - version : zrfisaac.batch.all : 26.8.30.3
 
 rem # [ batch ]
 
-rem # : - config - clone
+rem # : - config
 set c_clone=
 set c_clone=%c_clone% "https://github.com/zrfisaac/zrfisaac.github.io.git %~d0\zrfisaac"
-set c_clone=%c_clone% "https://github.com/zrfisaac/wiki.git %~d0\zrfisaac\wiki"
 set c_clone=%c_clone% "https://github.com/zrfisaac/zrfisaac.git %~d0\zrfisaac\github"
+set c_clone=%c_clone% "https://github.com/zrfisaac/private.git %~d0\zrfisaac\private"
 set c_clone=%c_clone% "https://github.com/zrfisaac/zrfisaac.github.io.git %~d0\zrfisaac\web"
+set c_clone=%c_clone% "https://github.com/zrfisaac/wiki.git %~d0\zrfisaac\wiki"
 
 rem # : - begin
 echo # - : %~dpnx0
 
-rem # : - all
-echo # . - all
+rem # : - all - public
+echo # . - all - public
 for %%z in (%c_clone%) do (
-	echo # . - all : %%z
+	echo # . - all - public : %%z
 	set _clone=%%z
 	set _clone=!_clone:"=!
 	git clone !_clone! >nul 2>&1
+)
+
+rem # : - config - private
+if exist %~d0\private\config.bat call %~d0\private\config.bat
+if exist %~d0\private\_config.bat call %~d0\private\_config.bat
+if exist %~d0\private\_.bat call %~d0\private\_.bat
+
+rem # : - all - private
+if exist (%~d0\private) (
+	echo # . - all - private
+	for %%z in (%c_clone%) do (
+		echo # . - all - private : %%z
+		set _clone=%%z
+		set _clone=!_clone:"=!
+		git clone !_clone! >nul 2>&1
+	)
 )
 
 rem # : - end
