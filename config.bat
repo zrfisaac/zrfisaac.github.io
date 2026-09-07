bb3518d947bca1c7bb7c54a87104b48503262a05
Isaac Caires Santana <zrfisaac@gmail.com>
2026-09-07 11:20:21 -0300

    zrfisaac : 26.9.7.1

--- config.bat
+++ config.bat
@@ -5,12 +5,12 @@ rem # [ about ]
 rem # - author : Isaac Caires Santana
 rem # . - email : zrfisaac@gmail.com
 rem # . - site : zrfisaac.github.io
-rem # - base : zrfisaac.batch.config : 26.8.30.2
 
 rem # [ batch ]
 
 rem # : - config - clone
 set c_clone=
+set c_clone=%c_clone% "https://github.com/zrfisaac/fork.assembler-simulator.git %~dp0fork\assembler-simulator"
 set c_clone=%c_clone% "https://github.com/zrfisaac/zrfisaac.git %~dp0github"
 set c_clone=%c_clone% "https://github.com/zrfisaac/zrfisaac.git %~dp0private"
 set c_clone=%c_clone% "https://github.com/zrfisaac/zrfisaac.github.io.git %~dp0web"
