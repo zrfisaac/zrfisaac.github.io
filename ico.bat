33e548c71a8c3ede7f34abbbba7f6207c5d6d64f
Isaac Caires Santana <zrfisaac@gmail.com>
2026-09-07 11:25:21 -0300

    zrfisaac.batch.ico : 26.9.7.1

---
+++ ico.bat
@@ -0,0 +1,78 @@
+@echo off
+chcp 65001 >nul
+setlocal enabledelayedexpansion
+rem # [ zrfisaac ]
+
+rem # [ about ]
+rem # - author : Isaac Caires Santana
+rem # . - email : zrfisaac@gmail.com
+rem # . - site : zrfisaac.github.io
+rem # - version : zrfisaac.batch.ico : 26.9.7.1
+
+rem # [ batch ]
+
+rem # : - config - chocolatey
+set c_chocolatey=
+set c_chocolatey=!c_chocolatey! imagemagick
+
+rem # : - config
+if exist %~dp0\config.bat call %~dp0\config.bat
+if exist %~dp0\_config.bat call %~dp0\_config.bat
+if exist %~dp0\_.bat call %~dp0\_.bat
+
+rem # : - variable
+set v_about=%~dp0\about.zrfi
+
+rem # : - variable
+set v_ytdlp=yt-dlp.exe
+for /f "delims=" %%i in ('where yt-dlp.exe 2^>nul') do set v_ytdlp=yt-dlp.exe
+if "%v_ytdlp%" equ "" (
+	set v_ytdlp=%v_ytdlp% C:\ProgramData\chocolatey\bin\yt-dlp.exe
+	for %%z in (%v_ytdlp%) do (
+		if exist %%z (
+			set v_ytdlp=%%z
+		)
+	)
+	if not exist "%v_ytdlp%" set v_ytdlp=
+)
+
+rem # : - variable
+set v_message=
+set v_administrator=0
+net session >nul 2>&1
+if "%errorlevel%" equ "0" set v_administrator=1
+set v_message=
+
+rem # : - begin
+echo # - : %~dpnx0
+
+rem # : - chocolatey
+if "%v_administrator%" neq "0" if "!v_message!" equ "" if not exist "C:\ProgramData\chocolatey" (
+	echo # . - chocolatey
+	PowerShell -command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
+	if exist "C:\ProgramData\chocolatey" set v_message=execute novamente
+)
+
+rem # : - package
+if "%v_administrator%" neq "0" if "!v_message!" equ "" (
+	echo # . - package
+	for %%z in (%c_chocolatey%) do (
+		if not exist "C:\ProgramData\chocolatey\lib\%%z" (
+			echo # . - package : %%z
+			choco install %%z --force --force-dependencies --confirm --ignore-checksums
+		)
+	)
+)
+
+rem # : - png
+if "%v_administrator%" equ "0" if "!v_message!" equ "" (
+	echo # . - png
+	for %%z in (*.png) do (
+		echo # . - png : %%z
+		magick "%%z" -define icon:auto-resize=128,96,64,48,32 "%%~nz.ico"
+	)
+)
+
+rem # : - end
+echo # . - end
+set /p _=
