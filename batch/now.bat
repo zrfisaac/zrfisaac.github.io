78fa806e654beb0912c91c9ee40ab5f8e42526f9
Isaac Caires Santana <zrfisaac@gmail.com>
2026-09-10 12:03:53 -0300

    zrfisaac.batch.now : 26.9.8.1

---
+++ batch/now.bat
@@ -0,0 +1,28 @@
+@echo off
+chcp 65001 >nul
+setlocal enabledelayedexpansion
+rem # [ zrfisaac ]
+
+rem # [ about ]
+rem # - author : Isaac Caires Santana
+rem # . - email : zrfisaac@gmail.com
+rem # . - site : zrfisaac.github.io
+rem # - version : zrfisaac.batch.now : 26.9.8.1
+
+rem # [ batch ]
+
+rem # : - config
+if exist %~dp0\config.bat call %~dp0\config.bat
+if exist %~dp0\_config.bat call %~dp0\_config.bat
+if exist %~dp0\_.bat call %~dp0\_.bat
+
+rem # : - begin
+echo # - : %~dpnx0
+
+rem # : - now
+echo # . - now
+powershell -NoProfile -ExecutionPolicy Bypass -Command "$_now = Get-Date; Get-ChildItem -LiteralPath '%CD%' -File -Force | ForEach-Object { $_.LastWriteTime = $_now }"
+
+rem # : - end
+echo # . - end
+set /p _=
