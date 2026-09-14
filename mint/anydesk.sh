83efe00eec71c0ed98c7087b91e0ed2fa66dce90
Isaac Caires Santana <zrfisaac@gmail.com>
2026-09-14 07:33:03 -0300

    zrfisaac.mint.anydesk : 26.9.14.1

---
+++ mint/anydesk.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires Santana
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+# - version : zrfisaac.mint.anydesk : 26.9.14.1
+
+# [ bash ]
+set -e
+sudo apt update
+sudo apt install -y ca-certificates curl apt-transport-https
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://keys.anydesk.com/repos/DEB-GPG-KEY -o /etc/apt/keyrings/keys.anydesk.com.asc
+sudo chmod a+r /etc/apt/keyrings/keys.anydesk.com.asc
+echo "deb [signed-by=/etc/apt/keyrings/keys.anydesk.com.asc] https://deb.anydesk.com all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list >/dev/null
+sudo apt update
+sudo apt install -y anydesk
+anydesk &
