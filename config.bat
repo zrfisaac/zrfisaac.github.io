5685bfd54f4f54b14a628816fb09c6fffdb9eadf
Isaac Caires Santana <zrfisaac@gmail.com>
2026-09-17 06:15:31 -0300

    zrfisaac : 26.9.16.4

--- config.bat
+++ config.bat
@@ -14,5 +14,7 @@ set c_clone=%c_clone% "https://github.com/zrfisaac/fork.assembler-simulator.git 
 set c_clone=%c_clone% "https://github.com/zrfisaac/zrfisaac.git %~dp0github"
 set c_clone=%c_clone% "https://github.com/zrfisaac/mvp.home-chef.git %~dp0mvp\home-chef"
 set c_clone=%c_clone% "https://github.com/zrfisaac/private.git %~dp0private"
+set c_clone=%c_clone% "https://github.com/zrfisaac/public.lfs.git %~dp0public\lfs"
+set c_clone=%c_clone% "https://github.com/zrfisaac/site.git %~dp0site"
 set c_clone=%c_clone% "https://github.com/zrfisaac/zrfisaac.github.io.git %~dp0web"
 set c_clone=%c_clone% "https://github.com/zrfisaac/wiki.git %~dp0wiki"
