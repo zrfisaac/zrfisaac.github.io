de36a48ef263288d6c72ce27a15688118999dbd8
Isaac Caires Santana <zrfisaac@gmail.com>
2026-09-26 09:16:02 -0300

    zrfisaac : 26.9.26.1

--- config.bat
+++ config.bat
@@ -18,6 +18,8 @@ set c_clone=%c_clone% "https://github.com/zrfisaac/site.git %~dp0site"
 set c_clone=%c_clone% "https://github.com/zrfisaac/zrfisaac.github.io.git %~dp0web"
 set c_clone=%c_clone% "https://github.com/zrfisaac/wiki.git %~dp0wiki"
 
+set c_clone=%c_clone% "https://github.com/zrfisaac/install.godot4.git %~dp0install\godot4"
+
 set c_clone=%c_clone% "https://github.com/zrfisaac/fork.assembler-simulator.git %~dp0fork\assembler-simulator"
 set c_clone=%c_clone% "https://github.com/zrfisaac/fork.ps4-v01.git %~dp0fork\ps4-v01"
 set c_clone=%c_clone% "https://github.com/zrfisaac/fork.ps4-v02.git %~dp0fork\ps4-v02"
